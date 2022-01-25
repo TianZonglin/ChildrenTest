9017bd3dc13b73bc1360660c66f8ed4bc1b25a1e
Glitch (glitch-hello-sqlite) <none>
2022-01-25 00:57:31 +0000

    👨‍👨‍👦📒 Checkpoint ./public/main.js:20633449/358 ./src/pages/index.hbs:20633449/7 ./public/style.css:20633449/84

--- public/main.js
+++ public/main.js
@@ -1,2 +1,20 @@
 $(function(){
+  
+  $("#p1a1").click(function(){
+    $(this).addClass("disabled");
+  });
+  
+  $("#p1a2").click(function(){
+    alert(1);
+  });
+  
+  $("#p1a3").click(function(){
+    alert(1);
+  });
+  
+  $("#p1a4").click(function(){
+    alert(1);
+  });
+
+  
 });
