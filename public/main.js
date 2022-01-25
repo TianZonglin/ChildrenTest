5b44ae9c1a9425d332551f9c067d2d8eab574cb6
Glitch (glitch-hello-sqlite) <none>
2022-01-25 01:20:11 +0000

    🚜🌏 Checkpoint ./public/main.js:20633449/378 ./src/pages/index.hbs:20633449/78

--- public/main.js
+++ public/main.js
@@ -1,13 +1,19 @@
 $(function(){
   
-  let oString = null;
-  function checkOrder(){
-     console.log(this);
+  let oString = "";
+  function checkOrder(str){
+    oString += str;
+    console.log(oString);
+    if(oString.length == 4 && oString === "健康是福"){
+      $("#p1a_text").html("<span>回答正确！</span>");
+    }else{
+      
+    }
     //alert($(this).val());
   }
   
   
-  $(".p1a").click(function(){$(this).addClass("disabled");checkOrder()});
+  $(".p1a").click(function(){$(this).addClass("disabled");checkOrder($(this).val())});
  
 
   
